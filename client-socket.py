9020eb8882fdfe9c281db1fd3ec2f7f3d609f64e
ThenoobMario <soni.nachiket@yahoo.in>
2021-01-08 17:29:06 +0530

    Init Commit

---
+++ client-socket.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+# Choosing a nickname
+nickname = input("Enter a nickname: ")
+
+# Connecting the client to the server
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(('127.0.0.1', 12458))
+
+# Listening to the server and Sending the nickname
+def receive():
+    while True:
+        try:
+            # Receive a message from the server (Not visible to user)
+            message = clientSocket.recv(1024).decode('ascii')
+
+            if message == 'NICK':
+                clientSocket.send(nickname.encode('ascii'))
+            else:
+                print('>' + message)
+        except:
+            # Close the connection if an error occurs
+            print('An error occured')
+            clientSocket.close()
+            break
+
+# A function which helps in writing messages
+def write():
+    while True:
+        message = '> {}: {}'.format(nickname, input('> '))
+        clientSocket.send(message.encode('ascii'))
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ server-socket.py
@@ -0,0 +1,63 @@
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 12458
+
+# Starting the Server
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((host, port))
+serverSocket.listen()
+
+# List for clients and their nicknames
+clients = []
+nicknames = []
+
+# We want to broadcast the message to all the clients connected on the Server
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            # As long as a message is received, we broadcast the message
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Cutting the connection to this client and removing it from the list
+            index = clients.index(client)
+            clients.remove(index)
+            client.close()
+
+            # Also remove the client's nickname and broadcast that the client left
+            nickname = nicknames[index]
+            broadcast(f'{nickname} has left the chat.'.encode('ascii'))
+            nicknames.remove(nickname)
+
+            break
+
+def receive():
+    while True:
+        # Accepting the client connection
+        client, address = serverSocket.accept()
+        print(f"Connected with {str(address)}")
+
+        # Requesting for a nickname from the client
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Broadcasting the message to all the clients in the server
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} joined the chat.'.encode('ascii'))
+        client.send('Connected to the server'.encode('ascii'))
+
+        # Creating thread for handling the client
+        # target is a callable object to be invoked
+        thread = threading.Thread(target = handle, args = (client,))
+        thread.start()  # Used to start a thread's activity
+
+print("The Server is listening.")
+receive()
